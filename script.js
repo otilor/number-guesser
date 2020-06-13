c35e20d0bbc78f48210460b65821e97bacd339e2
Gabriel Akinyosoye <gabrielfemi799>
2020-06-13 09:28:39 -0400

    Add footer information

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ const randomNumber = function () {
 
 const guessForm = document.getElementById("guessForm");
 const log = document.getElementById("log");
-guessForm.addEventListener("submit", processGuess)
+guessForm.addEventListener("submit", processGuess);
 
 function processGuess() {
     let userInput = document.getElementById("userInput").value;
-    log.textContent = "You submitted " + userInput
+    log.innerHTML = "You submitted " + userInput;
     event.preventDefault()
 }
-// lengthOfUserInput = userInput.length
-// alert(userInput.length)
-// alert(userInput)
